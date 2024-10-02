49718a22b6094682eb1b0a840a0a564fc56ac625
Nitin <nimbadkarnitin@gmail.com>
2024-10-02 23:39:19 +0530

    First Commit Ecommerce

---
+++ client/src/components/admin-view/AdminHeader.jsx
@@ -0,0 +1,42 @@
+import { AlignJustify, LogOut } from "lucide-react";
+import React from "react";
+import { Button } from "../ui/button";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "@/store/auth-slice";
+import { useToast } from "../hooks/use-toast";
+
+const AdminHeader = ({ open, setOpen }) => {
+  const dispatch = useDispatch();
+  const { toast } = useToast();
+  return (
+    <header className="flex items-center justify-between px-4 py-3 bg-background border-b">
+      <Button
+        onClick={() => {
+          setOpen(true);
+        }}
+        className="lg:hidden sm:block"
+      >
+        <AlignJustify />
+        <span className="sr-only">Toggle Menu</span>
+      </Button>
+      <div className="flex flex-1 justify-end">
+        <Button
+          onClick={() =>
+            dispatch(logoutUser()).then((d) => {
+              console.log(d, "-logout");
+              toast({
+                title: "Logged out",
+              });
+            })
+          }
+          className="inline-flex gap-2 rounded-md px-4 py-2 text-sm font-medium shadow items-center"
+        >
+          <LogOut />
+          <span>Logout</span>
+        </Button>
+      </div>
+    </header>
+  );
+};
+
+export default AdminHeader;
---
+++ client/src/store/auth-slice/index.js
@@ -0,0 +1,129 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isAuthenticated: false,
+  isLoading: true,
+  user: null,
+};
+
+export const registerUser = createAsyncThunk(
+  "/auth/register",
+
+  async (formData) => {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/api/auth/register`,
+      formData,
+      {
+        withCredentials: true,
+      }
+    );
+    return response.data;
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  "/auth/login",
+
+  async (formData) => {
+    const response = await axios.post(
+      `${import.meta.env.VITE_API_URL}/api/auth/login`,
+      formData,
+      {
+        withCredentials: true,
+      }
+    );
+    return response.data;
+  }
+);
+
+export const logoutUser = createAsyncThunk("/auth/logout", async () => {
+  const response = await axios.post(
+    `${import.meta.env.VITE_API_URL}/api/auth/logout`,
+    {},
+    {
+      withCredentials: true,
+    }
+  );
+  console.log(response.data,"-ss")
+  if(response.data) {
+    sessionStorage.clear();
+  }
+  return response.data;
+});
+
+export const checkAuth = createAsyncThunk(
+  "/auth/checkauth",
+
+  async () => {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_URL}/api/auth/check-auth`,
+      {
+        withCredentials: true,
+        headers: {
+          "Cache-Control":
+            "no-store, no-cache, must-revalidate, proxy-revalidate",
+        },
+      }
+    );
+    return response.data;
+  }
+);
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {},
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(registerUser.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(loginUser.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(loginUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = !action.payload.success ? null : action.payload.user;
+        state.isAuthenticated = !action.payload.success ? false : true;
+      })
+      .addCase(loginUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(checkAuth.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(checkAuth.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = !action.payload.success ? null : action.payload.user;
+        state.isAuthenticated = !action.payload.success ? false : true;
+      })
+      .addCase(checkAuth.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(logoutUser.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      });
+  },
+});
+
+export const { setUser } = authSlice.actions;
+export default authSlice.reducer;
